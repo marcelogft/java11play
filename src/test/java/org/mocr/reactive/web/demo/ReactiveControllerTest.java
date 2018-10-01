8ba96ece1fbb7ad7c0c14af9eb0f92085c1b5dfa
marcelogft <marcelo.colomer@gft.com>
2018-10-01 13:49:25 +0200

    test

---
+++ src/test/java/org/mocr/reactive/web/demo/ReactiveControllerTest.java
@@ -0,0 +1,36 @@
+package org.mocr.reactive.web.demo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mocr.reactive.web.demo.controller.ReactiveController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ReactiveController.class)
+public class ReactiveControllerTest {
+
+
+    private WebTestClient webTestClient;
+
+    @Before
+    public void initClient() {
+        webTestClient = WebTestClient.bindToController(ReactiveController.class).build();
+    }
+
+    @Test
+    public void testGetAllTweets() {
+        webTestClient.get().uri("/map")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBody().json("[{\"1\": \"one\",\"2\": \"two\"}]");
+    }
+
+}
